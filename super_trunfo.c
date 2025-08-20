80b66687bd4fad0663eff0d7c482f7faba458ea6
Guiverdectrl <abacateverde104@gmail.com>
2025-08-20 18:03:48 +0000

    alterações

--- super_trunfo.c
+++ super_trunfo.c
@@ -7,10 +7,11 @@ int main(){
     char Estado1[20] = "Recife";
     char Codigo1[4] = "A01";
     char Cidade1[20]= "joao pessoa";
-    int População1 = 650000;
-    float Area1 = 500.000;
-    float Pib1 = 50.000000;
+    int População1 = 893000;
+    float Area1 = 211.5;
+    float Pib1 = 22.244;
     int Pontos1 = 100;
+    float Densidade1, Pibpercapita1;
 
     //carta 2
     // Repeti o mesmo processo da carta 1.
@@ -18,28 +19,45 @@ int main(){
     char Codigo2[4] = "A02";
     char Cidade2[20] = "Maceio";
     int População2 = 957916;
-    float Area2 = 509.320;
-    float Pib2 = 2.7000000;
+    float Area2 = 509.32;
+    float Pib2 = 27.5;
     int Pontos2 = 200;
+    float Densidade2, Pibpercapita2;
 
     // Usei o printf para imprimir as variávei.
     // Usei os especificadores para sinalizar cada tipo de impressão.
-    printf("Carta 01: \n");
+     printf("Carta 01: \n");
+
     printf("Estado: %s \n", Estado1);
+
     printf("Código: %s \n", Codigo1);
+
+    printf("Cidade: %s \n", Cidade1);
+
     printf("População: %d \n", População1);
-    printf("Área: %f \n", Area1);
-    printf("PIB: %f \n", Pib1);
+
+    printf("Área: %.2f \n", Area1);
+
+    printf("PIB: %.2f \n", Pib1);
+
     printf("Pontos turísticos: %d \n\n", Pontos1);
 
     //Sendo %d para números inteiros, %s para nomes e codigos e a %f para imprimir números de pontos flutuantes.
 
     printf("Carta 02: \n");
+
     printf("Estado: %s \n", Estado2);
+
     printf("Código: %s \n", Codigo2);
+
+    printf("Cidade: %s \n", Cidade2);
+
     printf("População: %d \n", População2);
-    printf("Área: %f \n", Area2);
-    printf("PIB: %f \n", Pib2);
+
+    printf("Área: %.2f \n", Area2);
+
+    printf("PIB: %.2f \n", Pib2);
+
     printf("Pontos turísticos: %d \n", Pontos2);
 
     return (0);
