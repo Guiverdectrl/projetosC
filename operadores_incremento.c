0dfbde9fb210a374f5e07ea5aaa54d5c8fdfb9e4
Guiverdectrl <abacateverde104@gmail.com>
2025-08-20 15:46:14 +0000

    exercicios

---
+++ operadores_incremento.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int numero1 = 1;
+
+    printf("antes do incremento: %d\n", numero1);
+    numero1++;
+    printf("depois do incremento: %d\n", numero1);
+
+    numero1--;
+    printf("depois do decremento %d\n", numero1);
+
+    
+
+
+}
